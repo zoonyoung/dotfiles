8ccb155a8216a28bfea7d5bd902b55bdd6b3c8c8
Junyoung Park <zoonyoung@Junyoungs-MacBook-Pro.local>
2024-01-01 13:20:50 +0900

    customFolder

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,23 @@
+local base = require "plugins.configs.lspconfig"
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+local lspconfig = require "lspconfig"
+
+local servers = { "tsserver", "tailwindcss", "eslint" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,103 @@
+local plugins = {
+  -- liver-server
+  {
+    'barrett-ruth/live-server.nvim',
+    event = 'VeryLazy',
+    build = 'yarn global add live-server',
+    config = true,
+  },
+  -- none-ls
+  {
+    'nvimtools/none-ls.nvim',
+    event = 'VeryLazy',
+    opts = function()
+      return require 'custom.configs.null-ls'
+    end,
+  },
+  -- lspconfig
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      require 'plugins.configs.lspconfig'
+      require 'custom.configs.lspconfig'
+    end,
+  },
+  -- conform
+  {
+    'stevearc/conform.nvim',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
+    config = function()
+      require 'custom.configs.conform'
+    end,
+  },
+  --surround
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {}
+    end,
+  },
+
+  {
+    'abecodes/tabout.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('tabout').setup {
+        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        act_as_tab = true, -- shift content if tab out is not possible
+        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<C-d>', -- reverse shift default action,
+        enable_backwards = true, -- well ...
+        completion = true, -- if the tabkey is used in a completion pum
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = '`', close = '`' },
+          { open = '(', close = ')' },
+          { open = '[', close = ']' },
+          { open = '{', close = '}' },
+        },
+
+        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+        exclude = {}, -- tabout will ignore these filetypes
+      }
+    end,
+    wants = { 'nvim-treesitter' }, -- or require if not used so far
+    after = { 'nvim-cmp' }, -- if a completion plugin is using tabs load it before
+  },
+  -- rainbow
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require 'custom.configs.bracket'
+    end,
+  },
+  --import
+  { 'Galooshi/vim-import-js', event = 'VeryLazy' },
+  --cursorline
+  {
+    'yamatsum/nvim-cursorline',
+    event = 'VeryLazy',
+    opts = {
+      cursorline = {
+        enable = true,
+        timeout = 1000,
+        number = false,
+      },
+      cursorword = {
+        enable = true,
+        min_length = 3,
+        hl = { underline = true },
+      },
+    },
+  },
+  { 'mg979/vim-visual-multi', event = 'VeryLazy' },
+}
+
+return plugins
