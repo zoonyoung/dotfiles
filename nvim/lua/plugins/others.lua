631121e7f21a2418bb7990c37e400e683cd69596
zoonyoung <zoonyoung@zoonyoungs-Mac-Pro.local>
2024-07-03 17:42:27 +0900

    Add: nvim-ts-autotag install

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -1,5 +1,12 @@
 return {
   {
+    'windwp/nvim-ts-autotag',
+    requires = 'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+  {
     "lukas-reineke/indent-blankline.nvim",
     main = 'ibl',
     opts = {
